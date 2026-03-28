0662d78c4c000c653338a59cbd1372e65ec98b35
honey720 <honey720@kyonggi.ac.kr>
2026-03-28 21:03:01 +0900

    chore: initial commit

---
+++ src/test/java/com/honey720/websocket_stomp/WebsocketStompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honey720.websocket_stomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketStompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
